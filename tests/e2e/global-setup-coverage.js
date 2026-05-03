7b1e41387d33e54c194c148a1dba36d193547205
Efim <efimlu966@gmail.com>
2026-05-04 01:25:57 +0300

    rm junk

--- tests/e2e/global-setup-coverage.js
+++
@@ -1,9 +0,0 @@
-/**
- * Global setup for Playwright tests with automatic coverage collection
- * This file is automatically used when E2E_COVERAGE_DIR is set
- * No test modification needed!
- */
-
-module.exports = async () => {
-  // Global setup - nothing to do here, coverage is collected via fixture
-};
